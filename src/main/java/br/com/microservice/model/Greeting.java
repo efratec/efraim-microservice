b58332b95e0a05330ffe98b2047e2ccc553d89cc
Efraim Tenorio <efratec20.tenorio@gmail.com>
2021-06-21 23:45:54 -0300

    Initial commit of microservice project

---
+++ src/main/java/br/com/microservice/model/Greeting.java
@@ -0,0 +1,15 @@
+package br.com.microservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+    private Long id;
+    private String content;
+
+}
